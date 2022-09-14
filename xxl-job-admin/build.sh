825d145c45f0ac656af8e2095f8103bce9ea2230
longjie <815890067@qq.com>
2022-09-14 16:53:28 +0800

    修改Build.sh

--- xxl-job-admin/build.sh
+++ xxl-job-admin/build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+echo build image xxl-job-admin
+pwd
+
+# 到父工程目录
+cd ..
+pwd
+
+echo mvn clean
+mvn clean
+
+echo mvn install
+mvn install
+
+#回到项目目录
+cd xxl-job-admin
+pwd
+
+echo 创建 xxl-job-admin 镜像包
 image_tag=`date "+%Y%m%d%H%M%S"`
-#xxl:2.3.0
-image_name="47.100.207.207:8089/akso/xxljobadmin:$image_tag"
+image_name="172.29.250.56:8089/akso/xxljobadmin:$image_tag"
 
 docker build -t $image_name .
 
-docker login -u ci -p 1qaz2WSX 47.101.131.6:8089
+echo push $image_name
+docker login -u ci -p 1qaz2WSX 172.29.250.56:8089
 docker push $image_name
 
 echo $image_name
 
-#execute
